ad29ef448db22b4aadd3a799721c2bdeafeb91bf
vincentprata <76709142+vincentprata@users.noreply.github.com>
2024-06-13 02:47:35 -0400

    Changed wheel.h

--- wheel.h
+++ wheel.h
@@ -1,70 +1,29 @@
 #pragma once
 
 #include <vector>
-using namespace std;
 
-class Wheel
-{
-   vector<short>        lToR;
-   vector<short>        rToL;
+class Wheel {
+private:
+    std::vector<short> lToR;
+    std::vector<short> rToL;
+    const short nRec;
+    short notch;
+    short curPos;
+    unsigned int seed; // Add seed as a member variable
 
-   const short          nRec;
-   short               notch;
-   short              curPos;
+    void loadLToR();
+    void loadRToL();
+    void convertToOffset();
 
-   void loadLToR          ();
-   void loadRToL          ();
-   void convertToOffset   ();
- 
- public:
+public:
+    Wheel(short nEnts, unsigned int seed, short cPos);
+    ~Wheel();
 
-    Wheel
-    (
-    const short       nEnts,
-    const unsigned int seed,
-    const short        cPos
-    );
-
-   //
-   // Returns true
-   // if wheel is at
-   // notch, false otherwise.
-   //
-   inline bool atNotch() const  
-        { return (curPos == notch); }
-
-   //
-   // Set top of wheel
-   // to the specified
-   // value.
-   //
-   inline void resetCur(short cP) 
-                     { curPos = cP; }
-
-   //
-   // Get translated 
-   // value at index, i.
-   //
-   short getLtoR           (short i);
-   short getRtoL           (short i);
-
-   //
-   // Get current top
-   // of wheel position.
-   //
-   inline short getCurPos()
-                   { return curPos; }
-
-   //
-   // Advance the
-   // wheel one position.
-   //
-   inline void  advance()
-   { 
-      curPos = (++curPos >= nRec) ?
-                          0 : curPos;
-   }
-
-   ~Wheel();
-   
+    bool atNotch() const;
+    void resetCur(short cP);
+    short getLtoR(short i);
+    short getRtoL(short i);
+    short getCurPos() const;
+    void advance();
+    void reverse();
 };
