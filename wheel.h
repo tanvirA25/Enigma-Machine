0f79759ee70f751ad0c36cd6c07d0c801b1fc18b
tanvirA25 <113075133+tanvirA25@users.noreply.github.com>
2024-06-13 15:58:02 -0400

    Update wheel.h

--- wheel.h
+++ wheel.h
@@ -1,77 +1,81 @@
 #pragma once
 
 #include <vector>
+using namespace std;
 
 class Wheel
 {
-   vector<short>        lToR;
-   vector<short>        rToL;
+    vector<short>        lToR;
+    vector<short>        rToL;
 
-   const short          nRec;
-   short               notch;
-   short              curPos;
-   unsigned int       seed;
+    const short          nRec;
+    short               notch;
+    short              curPos;
 
-   void loadLToR          ();
-   void loadRToL          ();
-   void convertToOffset   ();
- 
- public:
+    void loadLToR();
+    void loadRToL();
+    void convertToOffset();
+
+public:
 
     Wheel
     (
-    const short       nEnts,
-    const unsigned int seed,
-    const short        cPos
+        const short       nEnts,
+        const unsigned int seed,
+        const short        cPos
     );
 
-   //
-   // Returns true
-   // if wheel is at
-   // notch, false otherwise.
-   //
-   inline bool atNotch() const  
-        { return (curPos == notch); }
+    //
+    // Returns true
+    // if wheel is at
+    // notch, false otherwise.
+    //
+    inline bool atNotch() const
+    {
+        return (curPos == notch);
+    }
 
-   //
-   // Set top of wheel
-   // to the specified
-   // value.
-   //
-   inline void resetCur(short cP) 
-                     { curPos = cP; }
+    //
+    // Set top of wheel
+    // to the specified
+    // value.
+    //
+    inline void resetCur(short cP)
+    {
+        curPos = cP;
+    }
 
-   //
-   // Get translated 
-   // value at index, i.
-   //
-   short getLtoR           (short i);
-   short getRtoL           (short i);
+    //
+    // Get translated 
+    // value at index, i.
+    //
+    short getLtoR(short i);
+    short getRtoL(short i);
 
-   //
-   // Get current top
-   // of wheel position.
-   //
-   inline short getCurPos()
-                   { return curPos; }
+    //
+    // Get current top
+    // of wheel position.
+    //
+    inline short getCurPos()
+    {
+        return curPos;
+    }
 
-   //
-   // Advance the
-   // wheel one position.
-   //
-   inline void  advance()
-   {  
-      curPos = (++curPos >= nRec) ?
-                          0 : curPos;
-   }
-   inline void reverse() {
-  
-    curPos = (--curPos < 0) ? nRec - 1 : curPos;
-}
+    //
+    // Advance the
+    // wheel one position.
+    //
+    inline void  advance()
+    {
+        curPos = (++curPos >= nRec) ?
+            0 : curPos;
+    }
+    inline void reverse() {
 
-Wheel::~Wheel(){
-
-}
+        curPos = (--curPos < 0) ? nRec - 1 : curPos;
+    }
 
 
+    ~Wheel();
 
+};
