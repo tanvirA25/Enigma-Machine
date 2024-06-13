f8452b2db501e50156fc9ab5b8072668c32cc128
tanvirA25 <113075133+tanvirA25@users.noreply.github.com>
2024-06-13 16:08:18 -0400

    additional fix

--- wheel.cpp
+++ wheel.cpp
@@ -1,86 +1,77 @@
 #include "wheel.h"
 #include<random>
-#include<cstdlib>
+
 
 using namespace std;
 
 Wheel::Wheel(
     const short       nEnts,
     const unsigned int seed,
-    const short        cPos) : nRec(nEnts), seed(seed),curPos(cPos)
-    {
-    
-    srand (seed);
-    
-    notch = rand() % nRec-1;
-        
-    loadLToR          ();
-    loadRToL          ();
-    convertToOffset   ();
- 
+    const short        cPos) : nRec(nEnts), curPos(cPos)
+{
 
-    }
+    srand(seed);
 
-void Wheel::loadLToR(){
-    const int rRange = nRec -1;
+    notch = rand() % nRec;
 
-    mt19937 gen(seed);
-    uniform_int_distribution<> dis(0,rRange);
+    loadLToR();
+    loadRToL();
+    convertToOffset();
 
-    for (short i = 0; i < nRec; ++i){
+}
+
+void Wheel::loadLToR() {
+
+    for (short i = 0; i < nRec; ++i) {
         lToR.push_back(i);
     }
-     for (short i = 0; i < nRec; ++i){ 
-        
-        short j = dis(gen);
+    for (short i = 0; i < nRec; ++i) {
+
+        short j = rand() % nRec;
         swap(lToR[i], lToR[j]);
     }
 
 }
 
-void Wheel::loadRToL(){
- const int rRange = nRec -1;
-    for (short i = 0; i < nRec; ++i){
+void Wheel::loadRToL() {
+    const int rRange = nRec - 1;
+    for (short i = 0; i < nRec; ++i) {
         rToL.push_back(0);
     }
-    for (short i = 0; i < nRec; ++i){ 
+    for (short i = 0; i < nRec; ++i) {
         rToL[lToR[i]] = i;
     }
 }
 
-void Wheel::convertToOffset(){
-    for (short i = 0; i < nRec; ++i){
+void Wheel::convertToOffset() {
+    for (short i = 0; i < nRec; ++i) {
         short offsett = lToR.at(i) - i;
         lToR[i] = offsett;
         offsett = rToL.at(i) - i;
         rToL[i] = offsett;
-
     }
 }
-
-short Wheel::getLtoR(short i){
+short Wheel::getLtoR(short i) {
     const short index = (curPos + i) % nRec;
     short val = lToR.at(index);
-    val+=i;
+    val += i;
     if (val < 0) val += nRec;
-    else if (val >= nRec) val-= nRec;
+    else if (val >= nRec) val -= nRec;
     return val;
 
 }
 
-short Wheel::getRtoL(short i){
+short Wheel::getRtoL(short i) {
     const short index = (curPos + i) % nRec;
     short val = rToL.at(index);
-    val+=i;
+    val += i;
     if (val < 0) val += nRec;
-    else if (val >= nRec) val-= nRec;
+    else if (val >= nRec) val -= nRec;
     return val;
 
 }
 
-
-Wheel::~Wheel(){
-    
-
-
+Wheel::~Wheel() {
+    lToR.clear();
+    rToL.clear();
 }
