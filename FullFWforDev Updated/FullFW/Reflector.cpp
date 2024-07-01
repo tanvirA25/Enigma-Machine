cee56cdf0def5318872471c03341ca7fec8c49c2
cronacat <vm359@njit.edu>
2024-07-01 15:41:23 -0400

    Fixed Reflector error.

--- FullFWforDev Updated/FullFW/Reflector.cpp
+++ FullFWforDev Updated/FullFW/Reflector.cpp
@@ -13,18 +13,19 @@ Reflector::Reflector() {
         81, 82, 31, 63, 84, 93, 67, 50, 91, 72, 57, 68, 54, 3, 78, 52,
         39, 10, 49, 22, 44, 69, 42, 64, 46, 60, 13, 9, 35, 87, 23, 94,
         85, 7, 50, 79, 41, 73, 2, 40, 32, 33, 83, 36, 64, 87, 13, 61,
-        35, 9, 33, 13, 40, 79, 22, 69, 56, 43, 37, 20
+        35, 9, 33, 13, 40, 79, 22, 69, 56, 43, 37, 20, 96
     };  // Does the array ever have to change?
 }
 
 const unsigned char Reflector::reflect(const unsigned char c) const {
-
+    printf("Reflect begin\n");
     if (c == 10) return 31;
     if (c == 31) return 10;
     if (c < OFFSET || c > LAST) {
         std::cout << "Bad value: " << c << "\nTERMINATING" << std::endl;
         std::exit(1);
     }
+    printf("gg\n");
     printf("Reflect %c to %c\n", c, numbers[c-32] + 32);
     return static_cast<unsigned char> (numbers[c-32] + 32);
 }
