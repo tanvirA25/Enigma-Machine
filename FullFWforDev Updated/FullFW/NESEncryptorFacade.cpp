af2f00e88dec9f3df51bd2dfb30593e892bbffe0
tanvirA25 <113075133+tanvirA25@users.noreply.github.com>
2024-07-01 23:23:27 -0400

    Update NESEncryptorFacade.cpp
    adjustment for reflector

--- FullFWforDev Updated/FullFW/NESEncryptorFacade.cpp
+++ FullFWforDev Updated/FullFW/NESEncryptorFacade.cpp
@@ -6,23 +6,29 @@ using namespace std;
 
 
 bool NESEncryptorFacade::encrypt(unsigned char& c) {
-	WHASS.advance();
+	
 	c = PB.getPBC(c);
 	c = WHASS.rToL(c);
+	c+=32;
 	c = REF.reflect(c);
+	c-=32;
 	c = WHASS.lToR(c);
 	c = PB.getPBC(c);
+	WHASS.advance();
 	
 	return true;
 
 }
 bool NESEncryptorFacade::decrypt(unsigned char& c) {
-	WHASS.advance();
+	
 	c = PB.getPBC(c);
+	c+=32;
 	c = WHASS.rToL(c);
 	c = REF.reflect(c);
+	c-=32;
 	c = WHASS.lToR(c);
 	c = PB.getPBC(c);
+	WHASS.advance();
 	
 	printf("End decrypt: %c\n", c);
 	return true;
