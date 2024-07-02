10b434339e2d310b12faddcb6a75c0c2b435c4e6
tanvirA25 <113075133+tanvirA25@users.noreply.github.com>
2024-07-02 16:16:35 -0400

    Update Plugboard.cpp
    added comments

--- FullFWforDev Updated/FullFW/Plugboard.cpp
+++ FullFWforDev Updated/FullFW/Plugboard.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include "Plugboard.h"
 
+// map for some characters
 Plugboard::Plugboard()
 {
 	plugs = { { 1,3 }, { 3,1 }, { 4,8 }, { 5,64 }, { 8,4 }, { 64,5 } };
@@ -11,17 +12,19 @@ Plugboard::~Plugboard() {}
 
 unsigned char Plugboard::getPBC(unsigned char c)
 {
+	// retruns bad value if out of range
 	//printf("Plugboard start: %c\n", c);
 	if (c < OFFSET || c > LAST) {
-	printf("Bad value: %c\nTERMINATING\n", c);
-	std::exit(1);
+		printf("Bad value: %c\nTERMINATING\n", c);
+		std::exit(1);
 }
+	// if there is no mapping return the original char
 	if (plugs.find(c) == plugs.end()) {
 		printf("No plug; %c\n", c);
 		return c;
 	}
-
-	printf("Plugboard changes %c to %c\n", c, plugs[c]);
+	// else return the changed char
+	// printf("Plugboard changes %c to %c\n", c, plugs[c]);
    return plugs[c];
 
 }
